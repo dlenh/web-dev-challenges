e05eecc98e9eb7ae8e4a60bbac4c8928dc74bfeb
David Lenh <dlenh425@gmail.com>
2022-10-07 12:42:23 -0700

    Double checked key word

--- String Week/2022-10-02.js
+++ String Week/2022-10-02.js
@@ -14,6 +14,7 @@ str4 = str3.split(" ").reverse().join(" ").split("").reverse().join("").split(" 
 
 // "Split this string up on *lowercase* 'V'. The first character in each substring besides this first one is what you need to remember for tomorrow's task. vS.A-#IYbSEkiQS5Sn@tq!CcG;+:^myl%CJcWzUZ;vpZQP*nH1KA*GRFI+B/xHR:><%&4.zf*2+Ey0x-#Kci LGcoOz3XKA)SAZ/j>T3sT=~kVs !6=B_Yei~@5~Gn)Q?Q7zI4yqfgXeTsdi9ga43?w^oC~KMqw8i02WHHm%-0QdTBJEum%@R_?,dJk 2t5QmgZRN8xW$fQ@Ge>tub)Ep1YjX()dpD3ru1(Q<8m?vaotN<SqFz-7z8fDWj7s|=UR*E:/CYLzj*?K:uCUmhkASg8;H^/%<HU%>=hhPpLPdb|fBAMvcA=)#4HH=Q^4nVHx^w,!T<11M~RQ.iI7Xk?lbtc d35csP&k1Xk2zk87NF^w691a4_xuveA=)#4HH=Q^4nVHx^w,!T<11M~RQ.iI7Xk?lbtc d35csP&k1Xk2zk87NF^w691a4_xu"
 
-str4.split("v").map(x => x[0]).slice(1).join("") === "Space"
+str5 = str4.split("v").map(x => x[0]).slice(1).join("")
+"Space"
 
 
